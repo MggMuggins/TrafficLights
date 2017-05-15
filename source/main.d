0ac427d04de464f82b11bab523bf8a221de66a65
MggMuggins <mggmugginsmc@gmail.com>
2017-05-15 09:42:40 -0500

    Functional example code

--- source/main.d
+++ source/main.d
@@ -3,8 +3,41 @@ module main;
 import renderer;
 import trafficlights;
 
+import consoled;
+import std.stdio;
+
 int main(string[] args)
 {
+    //Close handler (from ConsoleD docs) so that Ctrl-C actually works...
+    //This doesn't actually work...
+    addCloseHandler((i){
+        switch(i.type)
+        {
+            case CloseType.Other:
+                writeln("Other");
+                break;
+            
+            case CloseType.Interrupt:
+                writeln("Ctrl+C");
+                break;
+            
+            // Ctrl+Break for windows, Ctrl+Z for posix
+            case CloseType.Stop:
+                writeln("Ctrl+Break or Ctrl+Z");
+                break;
+            
+            // Posix only
+            case CloseType.Quit:
+                writeln(`Ctrl+\`);
+                break;
+            
+            default:
+                break;
+        }
+        
+        writeln(i.isBlockable);
+    });
+    
     /* Render Testing Stuffs
     auto render = new TileRenderer();
     render.registerTile(new CTile("Text", 4, 6, Color.initial, Color.red));
@@ -18,16 +51,28 @@ int main(string[] args)
     //*
     auto direction = new Intersection();
     direction.lightsFromFile("/usr/local/share/mggmuggins/lights.txt");
-    direction.setLightState("North", GREEN);
-    direction.setLightState("South", RED);
-    delay(6);
-    direction.setLightState("North", YELLOW);
-    delay(2);
     direction.setLightState("North", RED);
-    delay(1);
-    direction.setLightState("South", GREEN);
-    delay(6);
-    delay(2);
+    direction.setLightState("South", RED);
+    while(true)
+    {
+        direction.setLightState("East", GREEN);
+        direction.setLightState("West", GREEN);
+        delay(6);
+        direction.setLightState("East", YELLOW);
+        direction.setLightState("West", YELLOW);
+        delay(2);
+        direction.setLightState("East", RED);
+        direction.setLightState("West", RED);
+        delay(2);
+        direction.setLightState("North", GREEN);
+        direction.setLightState("South", GREEN);
+        delay(6);
+        direction.setLightState("North", YELLOW);
+        direction.setLightState("South", YELLOW);
+        delay(2);
+        direction.setLightState("North", RED);
+        direction.setLightState("South", RED);
+        delay(2);
+    }
     //*/
-    return 0;
 }
